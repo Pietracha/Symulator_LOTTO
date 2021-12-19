38c929f919ce8dc55a00b0858ea9ad94ada54a18
Piotr Milcarz <milcarz.piotr@gmail.com>
2021-12-19 02:18:13 +0100

    Symulator_LOTTO

--- symulator_lotto.py
+++ symulator_lotto.py
@@ -5,42 +5,39 @@ def input_function(i):
     return input_number
 
 
-def wywolanie():
-    lista_1 = []
-    lista_2 = []
-    lista_3 = []
+def lotto():
+    lista_1 = []  # Player's list
+    lista_2 = []  # Computer's list
+    lista_3 = []  # List of matching numbers
     for i in range (6):
         while True:
-            try:  #  Checking if input is of proper type
+            try:  # Checking if input is of a proper type
                 liczba = int(input_function(i))
             except ValueError:
                 print("Wrong type!")
                 continue
-            if 1 <= liczba <= 49: #  First checking if number is in range 1-49
-                if liczba not in lista_1: #  Then checking if number has been already entered
+            if 1 <= liczba <= 49: # First checking if number is in range 1-49
+                if liczba not in lista_1: # Then checking if number has been already entered
                     lista_1.append(liczba)
                     break
-                else:
-                    print("Number already entered!")
+                print("Number already entered!")
             else:
                 print("Number must be in range 1-49!")
-    lista_1.sort()
-    for _ in range (6):
+    lista_1.sort() # Sorting player's list ascending
+    for _ in range (6): # Creating computer's list
         while True:
             liczba_2 = randint(1,49)
-            if liczba_2 not in lista_2:
+            if liczba_2 not in lista_2: # Making sure that there are no two identical values in the list
                 lista_2.append(liczba_2)
                 break
-    lista_2.sort()
-    print(f"Player's list: ", lista_1)
-    print(f"Komputer's list: ", lista_2)
-    for y in range (len(lista_1)):
-        if lista_1[y] in lista_2:
-            lista_3.append(lista_1[y])
+    lista_2.sort() # Sorting computer's list
+    print("Player's list: ", lista_1)
+    print("Computer's list: ", lista_2)
+    for number in lista_1: # Cheking for hits
+        if number in lista_2:
+            lista_3.append(number)
         hits = len(lista_3)
     print(f"You have hit {hits} number(s)")
 
 
-wywolanie()
-
-
+lotto()
